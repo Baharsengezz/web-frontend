bb7d60301e2dda127aa1c9c94b7728f3f45f251b
Serbay ACAR <serbayacar@gmail.com>
2018-05-27 21:04:25 +0300

    navbar updated with bootstrap

--- src/components/layouts/navbar.js
+++ src/components/layouts/navbar.js
@@ -2,15 +2,38 @@ import React from 'react'
 
 const Nav = () =>{
     return(
-        <nav>
-            <ul>
-                <li>Ana Sayfa</li>
-                <li>Blog</li>
-                <li>Etkinlikler</li>
-                <li>Hakkımızda</li>
-                <li>İletişim</li>
-            </ul>
-        </nav>
+     
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="/">KodEvi</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul className="navbar-nav mr-auto">
+            <li className="nav-item active">
+              <a className="nav-link" href="/">Ana Sayfa <span className="sr-only">(current)</span></a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="/">Blog</a>
+            </li>
+             <li className="nav-item">
+              <a className="nav-link" href="/">Etkinlikler</a>
+            </li>
+             <li className="nav-item">
+              <a className="nav-link" href="/">Hakkımızda</a>
+            </li>
+             <li className="nav-item">
+              <a className="nav-link" href="/">İletişim</a>
+            </li>
+          </ul>
+          <form className="form-inline my-2 my-lg-0">
+            <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+            <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
     )
 };
 
